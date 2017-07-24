6fd81d0a347663c9e663e9573d3bbdbecb60c210
sanjaya <sanjaya@pmberjaya.com>
2017-07-24 17:03:02 +0700

    4

---
+++ Hans/Transaksi Penjualan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hans
+{
+    public partial class Transaksi_Penjualan : Form
+    {
+        public Transaksi_Penjualan()
+        {
+            InitializeComponent();
+        }
+    }
+}
